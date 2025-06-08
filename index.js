937f4d4df8b6f9b550d5e2e477159412bcd9f3a4
Vixito <carlosandres81182@gmail.com>
2025-06-08 17:05:38 -0500

    Exercise Part 3.4 Full Stack Open

--- index.js
+++ index.js
@@ -45,6 +45,17 @@ app.get('/api/persons/:id', (req, res) => {
   }
 });
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const personIndex = persons.findIndex(p => p.id === id);
+  if (personIndex !== -1) {
+    persons.splice(personIndex, 1);
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
